ca90711784c0de53b8e7dbc3cbb3c325142fc674
Hayden Telson <haydentelson@Haydens-MacBook-Pro.local>
2019-02-14 14:21:15 -0800

    Added 3 JSON events to myEvents and list

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
-
 /*
  * GET home page.
  */
 
- var data = require("../data.json")
+var data = require("../data.json")
 
 exports.view = function(req, res){
   res.render('index');
 };
 
 exports.viewList = function(req, res){
-  res.render('list');
+  res.render('list', {"cogsStudying": data.events[0], "freeFood": data.events[1], "improvShow": data.events[2]});
 };
 
 exports.viewCreate = function(req, res){
@@ -18,7 +17,7 @@ exports.viewCreate = function(req, res){
 };
 
 exports.viewMyEvents = function(req, res){
-  res.render('myEvents', {"event": data.event[0]});
+  res.render('myEvents', {"cogsStudying": data.events[0], "freeFood": data.events[1], "improvShow": data.events[2]});
 };
 
 exports.viewMap = function(req, res){
